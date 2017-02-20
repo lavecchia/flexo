42a1d3b00e39c3b1237c2ab066cb69c6bf0ee904
MartinL <lavecchia@users.noreply.github.com>
2017-02-20 17:38:22 -0300

    code cleaning

--- commonparameters.py
+++ commonparameters.py
@@ -1,21 +1,13 @@
 # common parameters
 
-from itertools import count
 KEYFHOF = "FINAL HEAT OF FORMATION"
 KEYCA = "COSMO AREA"
-KEYDEFAULT = "PUT KEYWORDS HERE"
-KEYCOSMO = "NR. ELEM.             COORDINATES     RADIUS  COSMO-CHARGE" #to position cursor in COSMO file
 MOPACPATH = "/opt/mopac/MOPAC2016.exe"
 DEFCORE = 3
-DEFPARAMFILE = "list_param.in"
-DEFREFFILE = "list_dgexp.in"
 DEFREPORFILE = "report.log"
 DEFSUMMARYFILE = "summaryfile.log"
-DEFTEMPLATEDIR= "templates"
 DEFINPUTEXT = ".mop"
 DEFTEMPLATEEXT = ".mop"
-DEFGASDIR = "gas"
-DEFOPTIMIZEPARAM = "optimize.log"
 RCONST=8.31 # gases constant in J K-1 mol-1
 TEMP = 298 # temperature in K
 #~ NS = 3.33679E+028 # numeral density in m-3
@@ -29,4 +21,4 @@ TMPDIR = "tmp/"
 BESTCONFNUM = 10 #"automatic" #number of best conformer stored
 ENERGYTHRESHOLD = 1.0 #difference in kcal/mol to calculate 
 RMSDTHRESHOLD = 0.3
-FINDROTATABLEPATH = "/home/mjl/Dropbox/CODIGOS/ConFind/findrotatable.pl"
+
